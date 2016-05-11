5ffe4373d8fecdeedb0575294b9b4d4d84a4006e
leejunhyeong <lhyoup0360@naver.com>
2016-05-11 17:17:42 +0900

    no message

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(session({
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jade');   ///
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
